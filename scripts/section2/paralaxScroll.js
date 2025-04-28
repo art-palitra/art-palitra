39bfec23df10bf4768a82bef1affa51cf6fc0f95
alexeyturalysov <alexeyturalysov@gmail.com>
2025-04-28 21:30:18 +0900

    update all

--- scripts/section2/paralaxScroll.js
+++ scripts/section2/paralaxScroll.js
@@ -1,5 +1,16 @@
-window.addEventListener('scroll', () => {
-    const scrollY = window.scrollY;
-    const image = document.querySelector('.scroll-img');
-    image.style.transform = `translateY(-${scrollY * 0.8}px)`;
-});
+
+
+const patalaxScroll = (value) => {
+    window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY;
+        const image = document.querySelector('.scroll-img');
+        image.style.transform = `translateY(-${scrollY * value}px)`;
+    });
+
+}
+
+if (document.documentElement.clientWidth < 900) {
+    patalaxScroll(0.9)
+} else {
+    patalaxScroll(0.9)
+}
